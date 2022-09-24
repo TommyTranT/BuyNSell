244b2a91406e8619f0f8455b31e2f6c50ff95c2c
Michael Binnick <mbinnick@live.ca>
2022-09-24 20:07:57 +0000

    writes seed file for listings test data insert

---
+++ db/seeds/02_listings.sql
@@ -0,0 +1,7 @@
+-- Users table seeds here (Example)
+INSERT INTO listings (title, description, price, owner_id)
+VALUES ('Tommy Truck', 'Its a big red truck!', 5000000, 1);
+INSERT INTO listings (title, description, price, owner_id)
+VALUES ('Nicks Car', 'Its a sweet speedster!', 10000000, 2);
+INSERT INTO listings (title, description, price, owner_id)
+VALUES ('Mikes Trike', 'Its a kind of lame kid bike...training wheels? Reall?', 5000, 3);
