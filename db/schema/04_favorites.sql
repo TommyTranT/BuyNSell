d30814abd992261f5ae96c6f165b7d5506f5f8b2
Michael Binnick <mbinnick@live.ca>
2022-09-24 19:37:45 +0000

    writes schema for 'favorites' database table

--- db/schema/04_favorites.sql
+++ db/schema/04_favorites.sql
@@ -1,9 +1,8 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+DROP TABLE IF EXISTS favorites CASCADE;
+CREATE TABLE favorites (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  listing_id INTEGER NOT NULL REFERENCES listings(id) ON DELETE CASCADE
 );
