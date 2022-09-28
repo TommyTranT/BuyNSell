3c4d9aef4fd954ce08bb4a7cca4a48de18be7abf
Tommy Tran <haitran1995@gmail.com>
2022-09-28 12:26:33 -0400

    Add delete button js

---
+++ public/scripts/listings.js
@@ -0,0 +1,25 @@
+$(() => {
+
+
+  $(".delete-button").click(function(){
+    alert("I noticed you copied my beard");
+
+    // // Gets key of the user
+    // const user_id = $("#user_id")[0].innerHTML;
+
+    // // Gets key of the listing
+    // const listing_id = $("#listing_id")[0].innerHTML;
+
+
+    // console.log(user_id, listing_id)
+    // // import function from index.js
+    // // databaseFn.addFavorite({user_id, listing_id})
+    // const favoriteKeys = {user_id, listing_id};
+
+    // $.post('/favorites', favoriteKeys, (response) => {
+    //   console.log("AJAX post went through");
+    })
+
+  });
+
+// });
