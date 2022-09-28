d7487691b136994989b3d6952b3534c62b48bcd6
Tommy Tran <haitran1995@gmail.com>
2022-09-28 13:00:47 -0400

    App acknowledges button input

---
+++ public/scripts/delete.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $(".delete-button").click(function(){
+    alert("You've clicked my heart");
+    console.log("ITS WORKINGS")
+
+    // // Gets key of the user
+    // const user_id = $("#user_id")[0].innerHTML;
+
+    // // Gets key of the listing
+    // const listing_id = $("#listing_id")[0].innerHTML;
+
+
+    // console.log(user_id, listing_id)
+    // // import function from index.js
+    // // databaseFn.addFavorite({user_id, listing_id})
+    // const favoriteKeys = {user_id, listing_id};
+
+    // $.post('/favorites', favoriteKeys, (response) => {
+    //   console.log("AJAX post went through");
+    // })
+
+  });
+});// END of Doc Ready
