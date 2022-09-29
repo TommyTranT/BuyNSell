d50bf3dfc524f4e19c63c7ab1a9c11471ea7594a
Michael Binnick <mbinnick@live.ca>
2022-09-29 17:48:57 +0000

    changes listings prices to be more reasonable

--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,7 +1,7 @@
 -- Listings table seeds here
 INSERT INTO listings (title, description, price, owner_id)
-VALUES ('Tommy Truck', 'Its a big red truck!', 5000000, 1);
+VALUES ('Tommy Truck', 'Its a big red truck!', 5000, 1);
 INSERT INTO listings (title, description, price, owner_id)
-VALUES ('Nicks Car', 'Its a sweet speedster!', 10000000, 2);
+VALUES ('Nicks Car', 'Its a sweet speedster!', 10000, 2);
 INSERT INTO listings (title, description, price, owner_id)
-VALUES ('Mikes Trike', 'Its a kind of lame kid bike...training wheels? Reall?', 5000, 3);
+VALUES ('Mikes Trike', 'Its a kind of lame kid bike...training wheels? Reall?', 50, 3);
