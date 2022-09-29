46ed3716355765ee41434d4d021ba78a6a98dc7c
Michael Binnick <mbinnick@live.ca>
2022-09-29 15:00:40 +0000

    adds time_sent to data seeds for messages

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,27 +1,27 @@
 -- Messages table seeds here
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (1, 2, 'That Mike guy sure is cool. I wish we were brothers. Btw, cool car, can I buy?', 2);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (2, 1, 'Yeah, Mike is okay. You can buy car, but it costs more now that I know you think Mike is cool. Double or nothin!', 2);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (1, 2, 'Fair, walked right into that one... Where you wanna meet?', 2);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (2, 1, 'Under the bridge downtown. See ya there!', 2);
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (1, 2, 'That Mike guy sure is cool. I wish we were brothers. Btw, cool car, can I buy?', 2, '2022-09-20 14:50:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (2, 1, 'Yeah, Mike is okay. You can buy car, but it costs more now that I know you think Mike is cool. Double or nothin!', 2, '2022-09-20 15:10:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (1, 2, 'Fair, walked right into that one... Where you wanna meet?', 2, '2022-09-20 15:22:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (2, 1, 'Under the bridge downtown. See ya there!', 2, '2022-09-20 15:24:00');
 
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (1, 3, 'I gotta have it', 3);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (3, 1, 'Do you have $$$?', 3);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (1, 3, 'I will literally pay anything', 3);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (3, 1, 'You know where to find me', 3);
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (1, 3, 'I gotta have it', 3, '2022-09-22 14:05:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (3, 1, 'Do you have $$$?', 3, '2022-09-22 14:15:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (1, 3, 'I will literally pay anything', 3, '2022-09-22 14:35:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (3, 1, 'You know where to find me', 3, '2022-09-22 15:07:00');
 
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (2, 1, 'Now that you bought my car, I need a ride. I like ur truck', 1);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (1, 2, 'Wow, and after overcharging me for the car?', 1);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (2, 1, '...', 1);
-INSERT INTO messages (sender_id, recipient_id, contents, listing_id)
-VALUES (1, 2, 'Fine. Im a good guy so its all good', 1);
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (2, 1, 'Now that you bought my car, I need a ride. I like ur truck', 1, '2022-09-25 14:07:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (1, 2, 'Wow, and after overcharging me for the car?', 1, '2022-09-25 14:23:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (2, 1, '...', 1, '2022-09-25 14:37:00');
+INSERT INTO messages (sender_id, recipient_id, contents, listing_id, time_sent)
+VALUES (1, 2, 'Fine. Im a good guy so its all good', 1, '2022-09-25 14:51:00');
