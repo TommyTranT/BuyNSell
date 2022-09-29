ba7791ec555cc7d8319b8cbd4f2ffb4df76fefef
Michael Binnick <mbinnick@live.ca>
2022-09-29 15:06:23 +0000

    adds time_sent column to getMessages select query in index.js

--- db/index.js
+++ db/index.js
@@ -265,7 +265,7 @@ exports.addFavorite = addFavorite;
 const getMessages = function(id) {
   return pool
     .query(`
-      SELECT listing_id, users.name as sender_name, listings.title as listing_title, contents
+      SELECT listing_id, users.name as sender_name, listings.title as listing_title, contents, time_sent
       FROM messages
       JOIN users on users.id = sender_id
       JOIN listings on listings.id = listing_id
