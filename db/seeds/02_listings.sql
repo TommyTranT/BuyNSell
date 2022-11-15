dd3d326c4c0b60f5ed4b3ec6eeffbf5ebc62ed47
Nicholas <nc_chau@hotmail.com>
2022-11-15 13:04:38 -0400

    rewrite seeds for listings, test lambdal replicate for generating ai image

--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,23 +1,23 @@
 -- Listings table seeds here
+INSERT INTO listings (title, description, price, owner_id, time_created, image_url)
+VALUES ('RhinoCyle', 'Intensely loyal.', 5000, 1, '2022-09-22 14:00:00', 'https://replicate.delivery/pbxt/sgxCJftAQgWgbSSZKrIxWrPBHiNdMdx8fbDn3XmwPjFcNXAQA/out-0.png');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Tommys OG Charizard', 'NM Condition - From original series, not open to trades. Price is firm.', 5000, 1, '2022-09-22 14:00:00');
+VALUES ('Octopip', 'Waits patiently for its prey.', 200, 2, '2022-09-18 12:01:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Nicks OG Blastoise', 'Slightly Played - Open to trades, or best offer', 200, 2, '2022-09-18 12:01:00');
+VALUES ('Termops', 'Capable of levelling a forest in moments.', 5, 3, '2022-09-19 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Mikes Shiny Jigglypuff', 'I would take candy as a trade. Is 5 bux too much?', 5, 3, '2022-09-19 17:27:00');
+VALUES ('Terrobat', 'The result of years of evolution.', 20, 5, '2022-09-07 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Bobs Diglett', 'Its a diglet card', 20, 5, '2022-09-07 17:27:00');
+VALUES ('Specida', 'Often confused about its surroundings.', 50, 4, '2022-09-15 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Francis Pikachu (fossil)', 'NM Condition - Open to trades, price firm.', 50, 4, '2022-09-15 17:27:00');
+VALUES ('Elecelope', 'Generates enough electricity to power a shopping mall on each breath.', 17, 3, '2022-09-13 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Haunter - XY Promo', 'Bad condition, looking for trades', 17, 3, '2022-09-13 17:27:00');
+VALUES ('Wallabyte', 'Takes down prey up to 5 times its size.', 150, 4, '2022-09-12 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Shiny Mewtwo', 'Got it from burger king years ago, not sure what its worth...pitch me!', 150, 4, '2022-09-12 17:27:00');
+VALUES ('Termitis', 'Very difficult to catch, hardly ever surfaces.', 1050, 5, '2022-08-01 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Fossil Mew', 'NM Condition - Got it from pokemon movie 2001!', 1050, 5, '2022-08-01 17:27:00');
+VALUES ('Badgord', 'Terrifying appearance but completely harmless to trainers.', 73, 2, '2022-09-01 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Bulbasaur', 'NM Condition - no trades, OBO', 73, 2, '2022-09-01 17:27:00');
+VALUES ('Skelora', 'Sharp teeth can penetrate plate steel with ease.', 35, 1, '2022-08-02 17:27:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Snorlax', 'Bad Condition - open to trades', 35, 1, '2022-08-02 17:27:00');
-INSERT INTO listings (title, description, price, owner_id, time_created)
-VALUES ('Starmie', 'I love this card...need money for coding bootcamp', 40, 3, '2022-09-11 17:27:00');
+VALUES ('Dinoseon', 'A noble creature from a lost age.', 40, 3, '2022-09-11 17:27:00');
